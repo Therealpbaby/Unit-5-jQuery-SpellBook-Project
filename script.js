9c506c1733b3517a3c6e2dca9bfec378deb30eba
Therealpbaby <43214461+Therealpbaby@users.noreply.github.com>
2019-03-06 09:11:33 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#hide").click(function(){
+ $(".purse").hide();
+});
+
+
+
+//Spell #2
+
+$("#fadeOut").click(function(){
+ $(".lipgloss").fadeOut();
+});
+
+
+
+//Spell #3
+
+$("#slideUp").click(function(){
+ $(".perfume").slideUp();
+});
+
+
+
